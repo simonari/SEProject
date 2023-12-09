aeccb465aac308e6c960e0c69b88d5d1101227fe
simonari <vsimonari@gmail.com>
2023-12-09 12:34:03 +0700

    Made connection to container

--- app/task_manager.py
+++ app/task_manager.py
@@ -4,7 +4,7 @@ import asyncio
 from .downloader import Downloader
 from .logger import logger
 
-celery = Celery(broker="pyamqp://localhost//")
+celery = Celery(broker="amqp://rabbitmq")
 loop = asyncio.get_event_loop()
 
 
@@ -16,3 +16,12 @@ def download(api: str, query: str):
     logger.info(f"({id(d)}): Download started")
     loop.create_task(d.run())
     # loop.run_in_executor(None, lambda: asyncio.run(d.run()))
+
+
+@celery.task(name="get_download")
+def get_download(api: str, query: str):
+    logger.info(f"Task received with query: {query} to {api} API")
+    d = Downloader(api, query)
+    logger.info(f"Downloader instantiated on {id(d)}")
+    logger.info(f"({id(d)}): Download started")
+    loop.run_until_complete(d.run())

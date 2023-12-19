127c74cd3bc7621ac83b77befac4305e7ebb690f
sengo <tikhomirov_ilya@mail.ru>
2023-12-19 15:43:20 +0700

    analyzer_v3

--- app/analyzer.py
+++ app/analyzer.py
@@ -1,9 +1,7 @@
 from .services import DataBaseManager
-import statistics as stat
 from datetime import datetime
 import numpy as np
 import scipy.interpolate
-import collections
 
 class BaseAnalyzer:
     def __init__(self):
@@ -17,87 +15,58 @@ class HHAnalyzer(BaseAnalyzer):
     async def analyze_salary(self, name: str):
         data = await self.db.get_data({
             "name": name,
-            #"salary_given": 1,
-            #"salary_empty": 0
+            "salary_given": 1,
+            "currency": "RUR"
         })
 
-        #не отрабатывает salary_given? бывают Nan в обоих полях
-        data = [s for s in data if ((s.salary_from is not None)
-                or (s.salary_to is not None))
-                and s.currency == "RUR" #анализ только зп в рублях
-                ]
-        #хранить зп буду в salary_from
-        for vac in data:
-            if (vac.salary_from is None) and (vac.salary_to is not None):
-                vac.salary_from = vac.salary_to
-                continue
-            if (vac.salary_from is not None) and (vac.salary_to is None):
-                continue
-            if (vac.salary_from is not None) and (vac.salary_to is not None):
-                vac.salary_from = (vac.salary_from + vac.salary_to) / 2
+       # data = [s for s in data if s.currency == "RUR"]
 
-
-        # подсчет средних
-        salaries = [s.salary_from for s in data]
-        if len(salaries) >= 1:
-            mean = stat.mean(salaries)
-            median = stat.median(salaries)
-        else:
-            mean = 0
-            median = 0
-            msg = "no data"
+        if len(data) < 2:
             return {
-                "amount": len(data),
-                "mean": mean,
-                "median": median,
-                "x": [],
-                "y": [], 
-                "msg": msg
+                "len": len(data),  # количество проанализированных вакансий
+                "msg": "not_enough_data"
             }
+        # хранить зп буду в salary_from
+        for vac in data:
+            if vac.salary_to is not None:
+                vac.salary_from = (vac.salary_from + vac.salary_to) / 2
+        #анализ средних
+        salaries = [s.salary_from for s in data]
+        mean = np.mean(salaries)
+        median = np.median(salaries)
+        # линейная интерполяция
+        # усредняем зарплаты в вакансиях с одинаковым временем
+        groups = {}
+        for vac in data:
+            time = vac.published_at
+            if time in groups:
+                groups[time].append(vac.salary_from)
+            else:
+                groups[time] = [vac.salary_from]
+        data_avg = []
+        for time, salaries in groups.items():
+            average_salary = sum(salaries) / len(salaries)
+            data_avg.append({"published_at": time, "salary": average_salary})
 
-        if len(salaries) >= 2:
-            # линейная интерполяция
-            # усредняем зарплаты в вакансиях с одинаковым временем
-            groups = {}
-            for vac in data:
-                time = vac.published_at
-                if time in groups:
-                    groups[time].append(vac.salary_from)
-                else:
-                    groups[time] = [vac.salary_from]
+        data_avg = sorted(data_avg, key=lambda vac: vac["published_at"])
+        # строим данные интерполяции
+        y = [s["salary"] for s in data_avg]
+        x = [s["published_at"].timestamp() for s in data_avg]
+        interpoler = scipy.interpolate.CubicSpline(x, y)
+        interpolation_count = 500 # число точек для графика
+        x = np.linspace(x[0], x[len(x) - 1], num=500).tolist()
+        y = list(interpoler(x))
 
-            data_avg = []
-            for time, salaries in groups.items():
-                average_salary = sum(salaries) / len(salaries)
-                data_avg.append({"published_at": time, "salary": average_salary})
-
-            data_avg = sorted(data_avg, key=lambda vac: vac["published_at"])
-            times = [s["published_at"] for s in data_avg]
-            x = [s.timestamp() for s in times]
-            y = [s["salary"] for s in data_avg]
-
-            interpoler = scipy.interpolate.CubicSpline(x, y)
-
-            interpolation_count = 1000 # число точек для графика
-            times = np.arange(times[0], times[len(times) - 1],
-                              (times[len(times) - 1] - times[0]) / interpolation_count,
-                              dtype=datetime).tolist()
-            x = [s.timestamp() for s in times]
-            y = list(interpoler(x))
-            msg = "success"
-        else:
-            times = []
-            y = []
-            msg = "no interoplation"
-
+        x = [datetime.fromtimestamp(s) for s in x]
         return {
             "len": len(data),                   # количество проанализированных вакансий
             "mean": mean,                       # среднеарифметическое зарплаты
             "median": median,                   # медиана зарплаты
-            "x": times,                         # метки времени интерполяции
+            "x": x,                             # метки времени интерполяции
             "y": y,                             # значения зарплат интерполяции
-            "msg": msg
+            "msg": "success"
         }
+
 
     async def analyze_experience(self, name: str):
         data = await self.db.get_data({

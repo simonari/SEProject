bf42bbb61c56820f06a8f5b0085ce83ba3b8d903
simonari <vsimonari@gmail.com>
2023-12-23 16:03:38 +0700

    Now analyzers producing images instead of numeric values

--- app/analyzer.py
+++ app/analyzer.py
@@ -2,6 +2,8 @@ from .services import DataBaseManager
 from datetime import datetime
 import numpy as np
 import scipy.interpolate
+from .models import Vacancy
+import matplotlib.pyplot as plt
 
 
 class BaseAnalyzer:
@@ -13,71 +15,90 @@ class HHAnalyzer(BaseAnalyzer):
     def __init__(self):
         super().__init__()
 
-    async def analyze_salary(self, name: str):
-        data = await self.db.get_data({
-            "name": name,
-            "salary_given": 1,
-            "currency": "RUR"
-        })
+    @staticmethod
+    def interpolation_data_preprocessing(data: list[Vacancy]):
+        data = [v for v in data if v.salary_from is not None]
 
         if len(data) < 2:
-            return {
-                "len": len(data),  # количество проанализированных вакансий
-                "msg": "not_enough_data"
-            }
-        # хранить зп буду в salary_from
-        for vac in data:
-            if vac.salary_to is not None:
-                vac.salary_from = (vac.salary_from + vac.salary_to) / 2
-        # анализ средних
-        salaries = [s.salary_from for s in data]
+            raise ValueError("Not enough data")
+
+        salaries = [v.salary_mean() for v in data]
         mean = np.mean(salaries)
         median = np.median(salaries)
-        # линейная интерполяция
-        # усредняем зарплаты в вакансиях с одинаковым временем
+
         groups = {}
-        for vac in data:
-            time = vac.published_at
+        for v in data:
+            time = v.published_at
+
             if time in groups:
-                groups[time].append(vac.salary_from)
-            else:
-                groups[time] = [vac.salary_from]
+                groups[time].append(v.salary_from)
+                continue
+
+            groups[time] = [v.salary_from]
+
         data_avg = []
         for time, salaries in groups.items():
             average_salary = sum(salaries) / len(salaries)
             data_avg.append({"published_at": time, "salary": average_salary})
 
         data_avg = sorted(data_avg, key=lambda v: v["published_at"])
-        # строим данные интерполяции
-        y = [s["salary"] for s in data_avg]
+
         x = [s["published_at"].timestamp() for s in data_avg]
+        y = [s["salary"] for s in data_avg]
+
+        return x, y, len(data), mean, median
+
+    async def analyze_salary(self, data: list[Vacancy]):
+        try:
+            x, y, vacancies_count, mean, median = self.interpolation_data_preprocessing(data)
+        except ValueError:
+            return {"msg": "Not enough data"}
+
         interpolator = scipy.interpolate.CubicSpline(x, y)
         interpolation_count = 500  # число точек для графика
+
         x = np.linspace(x[0], x[len(x) - 1], num=500).tolist()
         y = list(interpolator(x))
-
         x = [datetime.fromtimestamp(s) for s in x]
-        return {
-            "len": len(data),                   # количество проанализированных вакансий
-            "mean": mean,                       # среднеарифметическое зарплаты
-            "median": median,                   # медиана зарплаты
-            "x": x,                             # метки времени интерполяции
-            "y": y,                             # значения зарплат интерполяции
-            "msg": "success"
-        }
 
-    async def analyze_experience(self, name: str):
-        data = await self.db.get_data({
-            "name": name
-        })
+        fig, ax = plt.subplots()
+        ax.plot(x, y)
+        ax.axhline(mean, c="r", ls="--", label="Средняя зарплата")
+        ax.axhline(median, c="g", ls="--", label="Медианная зарплата")
+        ax.tick_params(axis='x', labelrotation=45)
+        fig.legend()
+        fig.savefig("salaries.png", bbox_inches="tight")
+
+        return "salaries.png"
+
+    @staticmethod
+    async def analyze_experience(data: list[Vacancy]):
         experience = [s.experience for s in data]
-        experience = dict((x, experience.count(x)) for x in set(experience) if experience.count(x) >= 1)
-        return {"experience": experience}
+        labels = list(set(experience))
+        values = [experience.count(x) for x in labels]
 
-    async def analyze_employment(self, name: str):
-        data = await self.db.get_data({
-            "name": name
-        })
+        fig, ax = plt.subplots()
+        ax.pie(values, labels=labels, autopct='%1.0f%%')
+
+        fig.savefig("experience.png", bbox_inches="tight")
+        return "experience.png"
+
+    @staticmethod
+    async def analyze_employment(data: list[Vacancy]):
         employment = [s.employment for s in data]
-        employment = dict((x, employment.count(x)) for x in set(employment) if employment.count(x) >= 1)
-        return {"employment": employment}
+        labels = list(set(employment))
+        values = [employment.count(x) for x in labels]
+
+        fig, ax = plt.subplots()
+        ax.pie(values, labels=labels, autopct='%1.0f%%')
+
+        fig.savefig("employment.png", bbox_inches="tight")
+        return "employment.png"
+
+    async def analyze(self, query_dict: dict[str, str]):
+        data = await self.db.get_data(query_dict)
+        return {
+            "salaries": await self.analyze_salary(data),
+            "experience": await self.analyze_experience(data),
+            "employment": await self.analyze_employment(data),
+        }
